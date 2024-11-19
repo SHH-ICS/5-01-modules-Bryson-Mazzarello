4a3812989d15a526c057c5915f97980b5b349634
Bryson <bryson.mazzarello@stu.ocsb.ca>
2024-11-19 15:51:48 +0000

    finished

--- Q1Pythagoras.py
+++ Q1Pythagoras.py
@@ -1,2 +1,10 @@
 # Create a program that will accept the two legs of a right-angle triangle, a and b, and display the length of the hypotenuse, c. 
 # Remember to use prompts for the input and labels for the output. Use the formula a2 + b2 = c2 to calculate your answer.
+import math
+
+while True:
+    try:
+       a = int(input()); b = int(input()); print(math.sqrt(a**2 + b**2))
+    except: 
+        print("Fail")
+        
--- Q4Picture.py
+++ Q4Picture.py
@@ -1,2 +1,26 @@
 # Using the pygame library, draw a simple picture. 
 # It can be anything you like, but you must use at least 2 different types of shapes and 3 different colors.
+
+import random
+
+import pygame
+
+from pygame.event import wait
+
+x = 750
+y = 400
+pygame.init()
+GameSurface = pygame.display.set_mode((x, y))
+for i in range(round(x*y/600)):
+    color = random.randint(0,5)
+    if color == 1:
+        color = "blue"
+    elif color == 2:
+        color = "red"
+    elif color == 3:
+        color = "green"
+    elif color == 4:
+        color = "yellow"
+    else:
+        color = "white"
+    pygame.draw.circle(GameSurface,color,(random.randint(0,x), random.randint(0,y)), x*y/10000)
