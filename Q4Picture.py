0260d1da3820963d2442bacf6d39b8e17775588d
Bryson <bryson.mazzarello@stu.ocsb.ca>
2024-11-21 10:08:12 -0500

    final

--- Q4Picture.py
+++ Q4Picture.py
@@ -4,15 +4,15 @@ import random
 from pygame.locals import QUIT
 
 x = 900
-y = 500
+y = x/2
 
-FPS = 16
+FPS = 15
 
 Clock = pygame.time.Clock()
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((x, y))
 DISPLAYSURF.fill(pygame.Color(135,0,0))
-pygame.display.set_caption('Idk')
+pygame.display.set_caption('Maholagoe')
 def Random():
    rndX = random.randint(0,x)
    rndY = random.randint(0,y)
@@ -29,8 +29,8 @@ while True:
             pygame.quit()
             sys.exit()
    Malev()
-   for nil in range(round(7*32/FPS)):
-      pygame.draw.line(DISPLAYSURF, (0,0,0), (Random()), (Random()),round(9/32*FPS+3))
-      pygame.draw.line(DISPLAYSURF, (255,255,255), (Random()), (Random()),round(6/32*FPS+3))
+   for nil in range(round(244/FPS)):
+      pygame.draw.line(DISPLAYSURF, (000,000,000), (Random()), (Random()),round(( 0.1*FPS)*( (x+y)/400+1)))
+      pygame.draw.line(DISPLAYSURF, (255,255,255), (Random()), (Random()),round(( 0.05*FPS)*( (x+y)/400+1)))
    Clock.tick(FPS)
    pygame.display.update()
